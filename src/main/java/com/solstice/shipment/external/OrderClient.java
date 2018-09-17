9a10e050bcdd2927164725ad80ae8725d72f99a9
Matt Nicolay <mnicolay@solstice.com>
2018-09-17 14:21:11 -0500

    add fallback methods to feign clients

--- src/main/java/com/solstice/shipment/external/OrderClient.java
+++ src/main/java/com/solstice/shipment/external/OrderClient.java
@@ -3,11 +3,21 @@ package com.solstice.shipment.external;
 import com.solstice.shipment.model.Order;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("order-order-line-service")
+@FeignClient(value = "order-order-line-service", fallback = OrderClientFallback.class)
 public interface OrderClient {
   @RequestMapping("/orders")
   List<Order> getOrdersByAccount(@RequestParam("accountId") long accountId);
+}
+
+@Component
+class OrderClientFallback implements OrderClient {
+
+  @Override
+  public List<Order> getOrdersByAccount(long accountId) {
+    return null;
+  }
 }
--- src/main/java/com/solstice/shipment/external/ProductClient.java
+++ src/main/java/com/solstice/shipment/external/ProductClient.java
@@ -2,11 +2,21 @@ package com.solstice.shipment.external;
 
 import com.solstice.shipment.model.Product;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("product-service")
+@FeignClient(value = "product-service", fallback = ProductClientFallback.class)
 public interface ProductClient {
   @RequestMapping("/products/{id}")
   Product getProductById(@PathVariable("id") long id);
+}
+
+@Component
+class ProductClientFallback implements ProductClient {
+
+  @Override
+  public Product getProductById(long id) {
+    return null;
+  }
 }
