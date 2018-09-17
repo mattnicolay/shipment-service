1adad5556aea1660cfbbcf910e765bc3b530aa4e
Matt Nicolay <mnicolay@solstice.com>
2018-09-17 15:55:56 -0500

    fix repository test by adding H2

--- src/test/java/com/solstice/shipment/dao/ShipmentRepositoryTest.java
+++ src/test/java/com/solstice/shipment/dao/ShipmentRepositoryTest.java
@@ -1,12 +1,16 @@
 package com.solstice.shipment.dao;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.*;
 
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.solstice.shipment.model.Shipment;
+import java.util.List;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,10 +18,26 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
     DbUnitTestExecutionListener.class})
 @DatabaseSetup("classpath:test-dataset.xml")
 public class ShipmentRepositoryTest {
 
+  @Autowired
+  ShipmentRepository shipmentRepository;
+
+  @Test
+  public void findAllByAccountIdOrderByDeliveryDate() {
+    List<Shipment> shipments = shipmentRepository.findAllByAccountIdOrderByDeliveryDate(1);
+
+    assertThat(shipments, is(notNullValue()));
+    assertFalse(shipments.isEmpty());
+    shipments.forEach(shipment -> {
+      assertThat(shipment.getId(), is(notNullValue()));
+      assertThat(shipment.getAccountId(), is(notNullValue()));
+      assertThat(shipment.getShippingAddressId(), is(notNullValue()));
+      assertThat(shipment.getShippedDate(), is(notNullValue()));
+      assertThat(shipment.getDeliveryDate(), is(notNullValue()));
+    });
+  }
 }
