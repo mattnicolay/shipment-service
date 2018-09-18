ad77a8bbf6e2dfbc1042e5069ce42cac75c1b117
Matt Nicolay <mnicolay@solstice.com>
2018-09-18 09:00:44 -0500

    add @EnableHystrix annotation

--- src/main/java/com/solstice/shipment/ShipmentServiceApplication.java
+++ src/main/java/com/solstice/shipment/ShipmentServiceApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableHystrix
 public class ShipmentServiceApplication {
 
   public static void main(String[] args) {
